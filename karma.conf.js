5332cc14aca8c21107a8a01b1796efca5cba41da
Callebe Gomes <callebe.gomes@ge.com>
2017-03-29 18:31:42 -0300

    starting the new sea 3.0

--- karma.conf.js
+++ karma.conf.js
@@ -13,9 +13,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        'bower_components/angular/angular.min.js',
-        'bower_components/angular-resource/angular-resource.min.js',
-        'bower_components/angular-mocks/angular-mocks.js',
+        'node_modules/angular/angular.min.js',
+        'node_modules/angular-mocks/angular-mocks.js',
         'src/**/*.js',
         'test/**/*.spec.js'
     ],
